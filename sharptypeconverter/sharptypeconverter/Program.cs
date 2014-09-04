1af446a25e89d21fc6c04ea6efe855fa70ed519c
Casper <c.folkers@xs4all.nl>
2014-09-04 14:18:40 +0200

    Added Folder conversion option to consoleApp, converts all .cs files in given folder.

--- sharptypeconverter/sharptypeconverter/Program.cs
+++ sharptypeconverter/sharptypeconverter/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace sharptypeconverter
 {
@@ -10,12 +8,30 @@ namespace sharptypeconverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter file location");
-            var file = Console.ReadLine();
-            
-            var output = Converter.Converter.Convert(System.IO.File.ReadAllText(file));
-            System.IO.File.WriteAllText(file + ".ts", output);
-
+            Console.WriteLine("enter file or folder location");
+            var path = Console.ReadLine();
+            if (path == null)
+            {
+                return;
+            }
+            if (File.Exists(path))
+            {
+                var output = Converter.Converter.Convert(File.ReadAllText(path));
+                File.WriteAllText(path + ".ts", output);
+            }
+            if (Directory.Exists(path))
+            {
+                var files = Directory.EnumerateFiles(path).Where(f => Path.GetExtension(f).Equals(".cs")).ToList();
+                var codes = files.Select(File.ReadAllText);
+                var typescriptCodes = Converter.Converter.Convert(codes.ToList()).ToList();
+                var newDirectpryPath = path + "Typescript";
+                Directory.CreateDirectory(newDirectpryPath);
+                for (var i = 0; i < typescriptCodes.Count(); i++)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(files.ElementAt(i));
+                    File.WriteAllText(newDirectpryPath + "\\" + fileName + ".ts", typescriptCodes.ElementAt(i));
+                }
+            }
         }
     }
 }
